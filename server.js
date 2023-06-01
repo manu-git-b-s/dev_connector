c936cdba65d55dac1975643648e93758dfb4fe3c
Manupriyan <manupriyan722@gmail.com>
2023-06-01 12:00:39 +0530

    Finished registering and authentcating user flow.

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const connectDb = require("./config/db");
+
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Connect database
+connectDb();
+
+// Init middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("API running");
 });
+
+// Define Routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   `server started on ${PORT}`;
